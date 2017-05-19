fb55d47a1c768cb7633e39a2af668bd9c76ca2c3
Seolhee Kim <seolhee@upspringmedia.com>
2017-05-18 18:55:24 -1000

    done

--- secret_number.js
+++ secret_number.js
@@ -1,4 +1,10 @@
 'use strict';
-module.exports = function() {
-  
+module.exports = function(num) {
+  var secretNumber = Math.floor(Math.random() * 10000);
+
+  return function (){
+
+    return secretNumber;
+  }
+
 };
